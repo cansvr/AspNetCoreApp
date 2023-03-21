ab6b5d60f85586df8f3783a98093f424cc376656
Can SEVER <can-svr@hotmail.com>
2023-03-21 18:09:40 +0300

    products eklendi

---
+++ AspNetCoreApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using AspNetCoreApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductRepository _productRepository;
+        public ProductController()
+        {
+            _productRepository = new ProductRepository();
+        }
+
+        public IActionResult Index()
+        {
+            var products = _productRepository.GetAll();
+            return View(products);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Update(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Remove(int id)
+        {
+            _productRepository.Remove(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
